736591aa5f6bc4be0e63418f77b7f3cb0b10d783
Dmitriy Bogach <dm.bogach@gmail.com>
2016-06-26 19:58:50 +0300

    Cleaned up

--- python lab2_1.py
+++ python lab2_1.py
@@ -1,24 +1,6 @@
 import math
 import sys
-# x=float(sys.argv[1])
-# m=float(sys.argv[2])
-# q=float(sys.argv[3])
 
-# a=q*(math.sqrt(2*math.pi))
-# b=math.pow((x-m),2)
-# c=math.pow(2*q,2)
-
-# f=(1/a)*(math.exp(-(b/c)))
-
-#f=(1/(q*math.sqrt(2*math.pi)))*math.exp(-(math.pow((x-m),2))/(math.pow(2*q,2)))
-
-#print f
-
-#print (1/(q*math.sqrt(2*math.pi)))*math.exp(-(x-m)**2/(2*q**2))
-
-
-# a = [1, 2, 3]
-# print a
 
 for i in range(1,10):
 	row = ""
